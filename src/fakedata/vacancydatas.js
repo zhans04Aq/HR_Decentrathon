8d9bade79da57bc8460211b71e5b0b05ba5c149d
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2024-10-19 21:11:08 +0500

    completed figma specialist page so far by design progress

---
+++ src/fakedata/vacancydatas.js
@@ -0,0 +1,44 @@
+import TCO from '../assets/icons/TCO.svg'
+
+export const jobData = [
+    {
+        id:1,
+        name: 'DevOps Engineer',
+        jobType: 'On-site',
+        shiftType:'Part Time',
+        jobDescription:'We are looking for a dynamic DevOps Engineer ...',
+        logoSrc:'../assets/icons/TCO.svg'
+    },
+    {
+        id:2,
+        name: 'Cybersecurity Risk Analyst',
+        jobType: 'Remote',
+        shiftType:'Full Time',
+        jobDescription:'We are looking for a dynamic CS Risk Analyst ...',
+        logoSrc:'../assets/icons/NCOC.svg'
+    },
+    {
+        id:3,
+        name: 'DevOps Engineer',
+        jobType: 'On-site',
+        shiftType:'Part Time',
+        jobDescription:'We are looking for a dynamic DevOps Engineer ...',
+        logoSrc:'../assets/icons/TCO.svg'
+    },
+    {
+        id:4,
+        name: 'DevOps Engineer',
+        jobType: 'On-site',
+        shiftType:'Part Time',
+        jobDescription:'We are looking for a dynamic DevOps Engineer ...',
+        logoSrc:'../assets/icons/NCOC.svg'
+    },
+    {
+        id:5,
+        name: 'DevOps Engineer',
+        jobType: 'On-site',
+        shiftType:'Part Time',
+        jobDescription:'We are looking for a dynamic DevOps Engineer ...',
+        logoSrc:'../assets/icons/TCO.svg'
+    },
+]
--- src/pages/ProfileJobSeeker.jsx
+++ src/pages/ProfileJobSeeker.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+
 
 import profileSearchIcon from '../assets/icons/profileSearchIcon.svg'
 import arrow from '../assets/icons/arrow.svg'
+import jobType from '../assets/icons/job-type.svg'
+import shiftType from '../assets/icons/shift-type.svg'
+
+import TCO from '../assets/icons/TCO.svg'
+import NCOC from '../assets/icons/NCOC.svg'
+
+import {jobData} from '../fakedata/vacancydatas'
 
 const ProfileJobSeeker = () => {
     return (
         <div className=''>
-            <div className='bg-[#2E00D7] w-full h-auto p-5 text-white flex flex-col gap-y-10'>
+            <section className='bg-[#2E00D7] w-full h-auto p-5 text-white flex flex-col gap-y-10'>
                 <div className='flex items-center justify-between'>
                     <div className='flex flex-row items-center gap-x-4 w-full'>
                         <div className='w-10 h-10 bg-gray-50 opacity-50 rounded-xl flex items-center justify-center'>
@@ -39,7 +46,59 @@ const ProfileJobSeeker = () => {
                         <p className='bg-[#D9D9D9] bg-opacity-25 py-3 px-4 rounded-xl whitespace-nowrap'>Hybrid</p>
                     </div>
                 </div>
-            </div>
+            </section>
+            <section className='pt-5 px-9'>
+                <div className='flex flex-row justify-between w-full'>
+                    <h1 className='text-base font-semibold'>Best Matches</h1>
+                    <p className='text-xs'>See All</p>
+                </div>
+                <div className='flex items-center gap-6 overflow-x-auto pb-4 text-center'>
+                {
+                    jobData.map((item)=>(
+                        <>
+                            <div key={item.id} className='bg-[#D9D9D9] pb-3 py-9 px-4 rounded-xl flex flex-col items-center gap-y-4 whitespace-nowrap'>
+                                <div className='bg-white px-5 pb-5 flex flex-col gap-y-4 w-full items-center rounded-xl'>
+                                    <div className='relative bottom-4 bg-[#D9D9D9] rounded-full p-3'>
+                                        <div className='bg-white rounded-full p-3'>
+                                            <img src={TCO} className='w-16 h-16 '/>
+                                        </div>
+                                    </div>
+                                    <h1>{item.name}</h1>
+                                    <div className='flex justify-around text-xs'>
+                                        <div className='flex items-center gap-x-2'>
+                                            <img src={jobType} className='w-4 h-4'/>
+                                            <p>{item.jobType}</p>
+                                        </div>
+                                        <div className='flex items-center gap-x-2'>
+                                        <img src={shiftType} className='w-4 h-4'/>
+                                            <p>{item.shiftType}</p>
+                                        </div>
+                                    </div>
+                                    <p className='text-xs w-1/2 self-start'>{item.jobDescription}</p>
+                                </div>
+                                <button className='bg-[#2E00D7] text-white w-2/3 py-2 rounded-lg'>Apply Now</button>
+                            </div>
+                        </>
+                    ))
+                }
+                </div>
+            </section>
+            <section className='pt-5 px-9 h-[50vh] bg-white'>
+                <div className=''>
+                    <div className='flex flex-row justify-between w-full'>
+                        <h1 className='text-base font-semibold'>Most Recent</h1>
+                        <p className='text-xs'>See All</p>
+                    </div>
+                </div>
+                <div className='flex items-center gap-6 overflow-x-auto pb-4'>
+                        <p className='bg-[#D9D9D9] bg-opacity-25 py-28 px-24 rounded-xl whitespace-nowrap'></p>
+                        <p className='bg-[#D9D9D9] bg-opacity-25 py-28 px-24 rounded-xl whitespace-nowrap'></p>
+                        <p className='bg-[#D9D9D9] bg-opacity-25 py-28 px-24 rounded-xl whitespace-nowrap'></p>
+                        <p className='bg-[#D9D9D9] bg-opacity-25 py-28 px-24 rounded-xl whitespace-nowrap'></p>
+                        <p className='bg-[#D9D9D9] bg-opacity-25 py-28 px-24 rounded-xl whitespace-nowrap'></p>
+
+                    </div>
+            </section>
         </div>
     );
 }
